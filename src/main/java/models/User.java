583b0500fb3385997c05def2e810a7ebd22d16c8
reinisrm <rreeiinniiss@gmail.com>
2023-12-27 18:53:12 +0200

    User model class created

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,23 @@
 package models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "user_table")
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+    @Column(name = "username")
+    private String username;
+    @Column(name = "password")
+    private String password;
 }
