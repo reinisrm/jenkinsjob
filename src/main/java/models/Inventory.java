490bf2a7312d82fd572e4828b45f75098d7b6ac0
reinisrm <rreeiinniiss@gmail.com>
2023-12-27 18:50:13 +0200

    Inventory model class created

--- src/main/java/models/Inventory.java
+++ src/main/java/models/Inventory.java
@@ -1,12 +1,12 @@
 package models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,7 +15,31 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Inventory {
     @Id
-    @Column(name = "inventory_id")
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int inventoryId;
+    @Column(name = "Datums")
+    private LocalDate date;
+    @Column(name = "Vards")
+    private String name;
+    @Column(name = "Uzvards")
+    private String surname;
+    @Column(name = "Telefona Nr.")
+    private String phoneNumber;
+    @Column(name = "Kurss")
+    private String courseName;
+    @Column(name = "Iekārta")
+    private String device;
+    @Column(name = "Inventāra numurs")
+    private String inventoryNumber;
+    @Column(name = "Kabinets")
+    private String room;
+    @Column(name = "vaiSanemts")
+    private boolean isReceived;
+    @Column(name = "vaiAtgriezts")
+    private boolean isReturned;
+    @Column(name = "Komentāri")
+    private String comments;
+
 
 }
