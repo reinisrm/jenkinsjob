2afd4260c907ac159ea7c08d39a8e7ee3177443a
reinisrm <rreeiinniiss@gmail.com>
2025-05-05 17:46:27 +0300

    Format upgrade, port change to 8081

--- src/main/java/com/example/inventorysystem/InventorySystemApplication.java
+++ src/main/java/com/example/inventorysystem/InventorySystemApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.InventorySystem")
@@ -16,10 +13,5 @@ public class InventorySystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventorySystemApplication.class, args);
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoderSimple() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 }
